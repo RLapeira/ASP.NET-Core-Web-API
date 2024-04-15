6bc1e1e47db4ef9ef5650bd2b40d90e586976b3a
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-15 09:54:20 +0200

    Demo: Returning Resources (Part 1)

---
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, Name = "New York City" },
+                    new { id = 2, Name = "Antwerp" }
+                });
+        }
+    }
+}
