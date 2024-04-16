1d922b895e48ac8a026f02d9453b1c25dcd1bbf9
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-16 11:22:25 +0200

    Demo: Working with Migrations

--- CityInfo/CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo/CityInfo.API/Entities/PointOfInterest.cs
@@ -13,6 +13,9 @@ namespace CityInfo.API.Entities
         [MaxLength(50)]
         public string Name { get; set; }
 
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
         [ForeignKey("CityId")]
         public City? City { get; set; }
         public int CityId { get; set; }
