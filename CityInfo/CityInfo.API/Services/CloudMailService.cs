b804fda224b9d6053493fb33fb2bda5c6905ec47
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-16 08:50:35 +0200

    Demo: Working with Configuration Files

--- CityInfo/CityInfo.API/Services/CloudMailService.cs
+++ CityInfo/CityInfo.API/Services/CloudMailService.cs
@@ -2,9 +2,14 @@
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        private string _mailTo = string.Empty;
+        private string _mailFrom = string.Empty;
 
+        public CloudMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
         public void Send(string subject, string message)
         {
             // send mail - output to console window
