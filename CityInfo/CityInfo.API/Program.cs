f979f8bf6ffb2fb7ed4e39c0da8f8b092134e5a4
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-17 11:48:41 +0200

    Demo: Using Information from the Token in an Authorization Policy

--- CityInfo/CityInfo.API/Program.cs
+++ CityInfo/CityInfo.API/Program.cs
@@ -75,6 +75,14 @@ builder.Services.AddAuthentication("Bearer")
         }
     );
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MustBeFromAntwerp", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("city", "Antwerp");
+    });
+});
 
 var app = builder.Build();
 
